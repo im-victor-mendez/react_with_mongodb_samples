3759861dcce7f5a61d84f30720dd8133b6058013
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-06 20:25:29 -0600

    ci: Implement database connection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 import { config } from "dotenv";
 import express, { json } from "express";
 import cors from "cors";
+import { connect } from "mongoose";
 
 config()
 
 const app = express()
 const port = process.env.PORT || 3000
+const databaseUri = process.env.DATABASE_URI
 
 app.use(json())
 app.use(cors())
+
+connect(databaseUri)
+.then(() => console.log(`Connected to MongoDB Database`))
+.catch(error => console.error(error))
 
 app.listen(port, () => console.log(`Listening in ${port}`))
