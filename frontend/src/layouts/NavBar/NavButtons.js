9066e6282f20874a34daf8f1ad390592d969007b
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-10 14:28:15 -0600

    style: Styled NavBar
    This modifies:
        - NavBar
        - colors
    
    This inits:
        - NavBar style
        - NavButtons

---
+++ frontend/src/layouts/NavBar/NavButtons.js
@@ -0,0 +1,44 @@
+const NavButtons = [
+    {
+      name: "Home",
+      to: '/'
+    },
+    {
+      name: "AirBnb",
+      to: '/airbnb'
+    },
+    {
+        name: "Analytics",
+        to: '/analytics'
+    },
+    {
+        name: "Geo Spatial",
+        to: '/geospatial'
+    },
+    {
+        name: "Guides",
+        to: '/guides'
+    },
+    {
+        name: "Mflix",
+        to: '/mflix'
+    },
+    {
+        name: "Restaurants",
+        to: '/restaurants'
+    },
+    {
+        name: "Supplies",
+        to: '/supplies'
+    },
+    {
+        name: "Training",
+        to: '/training'
+    },
+    {
+        name: "Weather Data",
+        to: '/geospatial'
+    }
+]
+
+export default NavButtons
