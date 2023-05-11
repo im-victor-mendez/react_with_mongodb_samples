72540d507a9d5c72642abb987da867cc9ebd8e29
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-10 19:11:00 -0600

    feat: Implemented analytics customers route and controller

--- backend/routes/analytics.routes.js
+++ backend/routes/analytics.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getAnalyticsAccounts } from "./controllers/analytics.controller.js";
+import { getAnalyticsAccounts, getAnalyticsCustomers } from "./controllers/analytics.controller.js";
 
 const analyticsRouter = Router()
 const path = '/analytics'
 
 const accounts = `${path}/accounts`
+const customers = `${path}/customers`
 
 analyticsRouter.use(accounts, getAnalyticsAccounts)
+analyticsRouter.use(customers, getAnalyticsCustomers)
 
 export default analyticsRouter
--- backend/routes/controllers/analytics.controller.js
+++ backend/routes/controllers/analytics.controller.js
@@ -2,25 +2,60 @@ import connection from "../../database.js";
 import { pageSize } from "./constants.js";
 
 const db = connection.db('sample_analytics')
-const accountsCollection = await db.collection('accounts')
 
 export async function getAnalyticsAccounts(req, res) {
     let { page, account_id, limit, product } = req.query
 
     page = parseInt(page) || 1
-    account_id = parseInt(account_id) || null
-    limit = parseInt(limit) || null
-    product = product || null
+    account_id = parseInt(account_id) || undefined
+    limit = parseInt(limit) || undefined
     
     const skip = (page - 1) * pageSize
 
     const filter = { account_id, limit, products: {$in: [ product ] } }
 
-    if (!account_id) delete filter.account_id
-    if (!limit) delete filter.limit
-    if (!product) delete filter.products
+    Object.keys(filter).forEach(key => {
+        if (filter[key] === undefined) delete filter[key]
+    })
+
+    if (product === undefined) delete filter.products
     
+    const accountsCollection = await db.collection('accounts')
     const cursor = await accountsCollection.find(filter).skip(skip).limit(pageSize).toArray()
+
+    res.send(cursor)
+}
+
+export async function getAnalyticsCustomers(req, res) {
+    let { page, account, address, birthdate, email, name, tier, username } = req.query
+
+    page = parseInt(page) || 1
+    const skip = (page - 1) * pageSize
+
+    account = parseInt(account) || undefined
+
+    if (name !== undefined) name = new RegExp(name, 'ig')
+    if (address !== undefined) address = new RegExp(address, 'ig')
+    if (email !== undefined) email = new RegExp(email, 'ig')
+
+    const filter = { username,
+        name: { $in: [name] },
+        address: { $in: [address] },
+        email: { $in: [email] },
+        accounts: { $in: [account] }
+    }
+
+    Object.keys(filter).forEach(key => {
+        if (filter[key] === undefined) delete filter[key]
+    })
+
+    if (name === undefined) delete filter.name
+    if (address === undefined) delete filter.address
+    if (account === undefined) delete filter.accounts
+    if (email === undefined) delete filter.email
+
+    const customersCollection = await db.collection('customers')
+    const cursor = await customersCollection.find(filter).limit(pageSize).toArray()
 
     res.send(cursor)
 }
